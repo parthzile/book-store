3adeef3922b8bf31340e9b14e35af2f3fcca4ea7
Parth Zile <parthzile01@gmail.com>
2023-09-11 13:40:23 +0530

    Route for Get all books from database

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,24 @@ app.get('/' , (request, response) => {
     return response.status(234).send('Welcome TO MERN Stack')
 });
 
+//Route for Get all books from database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books,
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+
 //Route for Save a new Book
-app.post('/book', async(request,response) => {
+app.post('/books', async(request,response) => {
     try {
         if(
             !request.body.title ||!request.body.author ||!request.body.publishYear
