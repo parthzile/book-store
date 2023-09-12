7f3b178f2aa2cd0527876a3fe565e1a08fe3a4e2
Parth Zile <parthzile01@gmail.com>
2023-09-12 17:21:27 +0530

    Route for Update a Book

--- backend/index.js
+++ backend/index.js
@@ -97,6 +97,26 @@ app.put('/books/:id' , async(request,response) => {
     }
 });
 
+
+//Route for Delete a Book
+app.delete('/books/:id', async(request,response) => {
+    try {
+        const {id} = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message: 'Book not found'});
+        }
+
+        return response.status(200).send({ message: 'Book deleted successfully'})
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
